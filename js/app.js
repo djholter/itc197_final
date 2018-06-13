3529c73169fe483fdca9087bed7e070a26f03e49
David Holter <djholter@gmail.com>
2018-06-13 23:29:20 +0000

    initial push of files.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/* global $ */
+
+$(document).ready(function() {
+    let menu = $('nav > ul'),
+        link = $('#mobileMenu');
+    
+    link.on('click', function() {
+        if (menu.hasClass('closed')) {
+            menu.removeClass('closed');
+            menu.addClass('open');
+        } else {
+            menu.removeClass('open');
+            menu.addClass('closed');
+        }
+    });
+});
